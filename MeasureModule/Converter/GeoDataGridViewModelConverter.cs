d171d68afb5e38361870b4934625bfae1f35d56d
joncash <tw.kchsieh@gmail.com>
2014-02-20 17:45:57 +0800

    修正 Namespace 的引用

--- MeasureModule/Converter/GeoDataGridViewModelConverter.cs
+++ MeasureModule/Converter/GeoDataGridViewModelConverter.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ViewROI;
+using ViewROI.Model;
 
 namespace MeasureModule
 {
--- MeasureModule/Converter/ProgGraphicModelConverter.cs
+++ MeasureModule/Converter/ProgGraphicModelConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using ViewROI;
+using ViewROI.Model;
 namespace MeasureModule
 {
 	/// <summary>
@@ -12,13 +13,13 @@ namespace MeasureModule
 	{
 		public ROIProgDistance ConvertToROIProgDistance(ProgGraphicModel raw)
 		{
-			ROIProgDistance model = new ROIProgDistance();
+			ROIProgDistance model = new ROIProgDistance(raw);
 			return model;
 		}
 
 		public ROIProgCircle ConvertToROIProgCircle(ProgGraphicModel raw)
 		{
-			ROIProgCircle model = new ROIProgCircle();
+			ROIProgCircle model = new ROIProgCircle(raw);
 			return model;
 		}
 	}
