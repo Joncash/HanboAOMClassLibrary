52da992983f2ec0e9c586339b372016d2f947451
joncash <tw.kchsieh@gmail.com>
2014-04-21 22:50:49 +0800

    (^) 新增參考座標 for 歸零

---
+++ Hanbo.WindowControlWrapper/RefCoordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hanbo.WindowControlWrapper
+{
+	public class RefCoordinate
+	{
+		/// <summary>
+		/// 參考座標 ID
+		/// </summary>
+		public string ID { get; set; }
+
+		/// <summary>
+		/// 顯示名稱
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// 描述
+		/// </summary>
+		public string Desc { get; set; }
+	}
+}
