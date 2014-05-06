7ec3d39f3bae6d9b44c2c2634f9809d9802bfee8
joncash <tw.kchsieh@gmail.com>
2014-05-06 11:16:27 +0800

    (+) GridView 與 TreeView 資料顯示自訂控制項

---
+++ Hanbo.WindowsFormsControlLibrary/UserControls/GeoDataUserControl.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Hanbo.WindowsFormsControlLibrary.UserControls
+{
+	partial class GeoDataUserControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeoDataUserControl));
+			this.ContentPanel = new System.Windows.Forms.Panel();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.GeoDataGridView = new System.Windows.Forms.DataGridView();
+			this.GeoTreeViewGroupBox = new System.Windows.Forms.GroupBox();
+			this.GeoTreeView = new System.Windows.Forms.TreeView();
+			this.GeoToolStrip = new System.Windows.Forms.ToolStrip();
+			this.UnitComboBox = new System.Windows.Forms.ToolStripComboBox();
+			this.UnitComboBoxLabel = new System.Windows.Forms.ToolStripLabel();
+			this.CoordinateComboBox = new System.Windows.Forms.ToolStripComboBox();
+			this.CoordinateLabel = new System.Windows.Forms.ToolStripLabel();
+			this.ExportButton = new System.Windows.Forms.ToolStripButton();
+			this.ClearListButton = new System.Windows.Forms.ToolStripButton();
+			this.GeoContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.SetCoordinateSystemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.ContentPanel.SuspendLayout();
+			this.statusStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.GeoDataGridView)).BeginInit();
+			this.GeoTreeViewGroupBox.SuspendLayout();
+			this.GeoToolStrip.SuspendLayout();
+			this.GeoContextMenuStrip.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// ContentPanel
+			// 
+			this.ContentPanel.BackColor = System.Drawing.SystemColors.Control;
+			this.ContentPanel.Controls.Add(this.statusStrip1);
+			this.ContentPanel.Controls.Add(this.splitContainer1);
+			this.ContentPanel.Controls.Add(this.GeoToolStrip);
+			resources.ApplyResources(this.ContentPanel, "ContentPanel");
+			this.ContentPanel.Name = "ContentPanel";
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			resources.ApplyResources(this.statusStrip1, "statusStrip1");
+			this.statusStrip1.Name = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			resources.ApplyResources(this.toolStripStatusLabel1, "toolStripStatusLabel1");
+			// 
+			// splitContainer1
+			// 
+			resources.ApplyResources(this.splitContainer1, "splitContainer1");
+			this.splitContainer1.Name = "splitContainer1";
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.BackColor = System.Drawing.Color.Green;
+			this.splitContainer1.Panel1.Controls.Add(this.GeoDataGridView);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.Control;
+			this.splitContainer1.Panel2.Controls.Add(this.GeoTreeViewGroupBox);
+			// 
+			// GeoDataGridView
+			// 
+			this.GeoDataGridView.AllowUserToAddRows = false;
+			this.GeoDataGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.GeoDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			resources.ApplyResources(this.GeoDataGridView, "GeoDataGridView");
+			this.GeoDataGridView.Name = "GeoDataGridView";
+			// 
+			// GeoTreeViewGroupBox
+			// 
+			this.GeoTreeViewGroupBox.Controls.Add(this.GeoTreeView);
+			resources.ApplyResources(this.GeoTreeViewGroupBox, "GeoTreeViewGroupBox");
+			this.GeoTreeViewGroupBox.Name = "GeoTreeViewGroupBox";
+			this.GeoTreeViewGroupBox.TabStop = false;
+			// 
+			// GeoTreeView
+			// 
+			resources.ApplyResources(this.GeoTreeView, "GeoTreeView");
+			this.GeoTreeView.Name = "GeoTreeView";
+			// 
+			// GeoToolStrip
+			// 
+			this.GeoToolStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.GeoToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UnitComboBox,
+            this.UnitComboBoxLabel,
+            this.CoordinateComboBox,
+            this.CoordinateLabel,
+            this.ExportButton,
+            this.ClearListButton});
+			resources.ApplyResources(this.GeoToolStrip, "GeoToolStrip");
+			this.GeoToolStrip.Name = "GeoToolStrip";
+			// 
+			// UnitComboBox
+			// 
+			this.UnitComboBox.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.UnitComboBox.Name = "UnitComboBox";
+			resources.ApplyResources(this.UnitComboBox, "UnitComboBox");
+			// 
+			// UnitComboBoxLabel
+			// 
+			this.UnitComboBoxLabel.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.UnitComboBoxLabel.AutoToolTip = true;
+			this.UnitComboBoxLabel.Margin = new System.Windows.Forms.Padding(0, 1, 3, 2);
+			this.UnitComboBoxLabel.Name = "UnitComboBoxLabel";
+			resources.ApplyResources(this.UnitComboBoxLabel, "UnitComboBoxLabel");
+			// 
+			// CoordinateComboBox
+			// 
+			this.CoordinateComboBox.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.CoordinateComboBox.Margin = new System.Windows.Forms.Padding(1, 0, 15, 0);
+			this.CoordinateComboBox.Name = "CoordinateComboBox";
+			resources.ApplyResources(this.CoordinateComboBox, "CoordinateComboBox");
+			// 
+			// CoordinateLabel
+			// 
+			this.CoordinateLabel.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.CoordinateLabel.Margin = new System.Windows.Forms.Padding(0, 1, 3, 2);
+			this.CoordinateLabel.Name = "CoordinateLabel";
+			resources.ApplyResources(this.CoordinateLabel, "CoordinateLabel");
+			// 
+			// ExportButton
+			// 
+			this.ExportButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.ExportButton.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.ExportFile;
+			resources.ApplyResources(this.ExportButton, "ExportButton");
+			this.ExportButton.Name = "ExportButton";
+			// 
+			// ClearListButton
+			// 
+			this.ClearListButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.ClearListButton.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.clearList_32;
+			resources.ApplyResources(this.ClearListButton, "ClearListButton");
+			this.ClearListButton.Name = "ClearListButton";
+			// 
+			// GeoContextMenuStrip
+			// 
+			this.GeoContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.SetCoordinateSystemToolStripMenuItem});
+			this.GeoContextMenuStrip.Name = "GeoContextMenuStrip";
+			resources.ApplyResources(this.GeoContextMenuStrip, "GeoContextMenuStrip");
+			// 
+			// SetCoordinateSystemToolStripMenuItem
+			// 
+			this.SetCoordinateSystemToolStripMenuItem.Name = "SetCoordinateSystemToolStripMenuItem";
+			resources.ApplyResources(this.SetCoordinateSystemToolStripMenuItem, "SetCoordinateSystemToolStripMenuItem");
+			this.SetCoordinateSystemToolStripMenuItem.Click += new System.EventHandler(this.SetCoordinateSystemToolStripMenuItem_Click);
+			// 
+			// GeoDataUserControl
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.ContentPanel);
+			this.Name = "GeoDataUserControl";
+			this.ContentPanel.ResumeLayout(false);
+			this.ContentPanel.PerformLayout();
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.GeoDataGridView)).EndInit();
+			this.GeoTreeViewGroupBox.ResumeLayout(false);
+			this.GeoToolStrip.ResumeLayout(false);
+			this.GeoToolStrip.PerformLayout();
+			this.GeoContextMenuStrip.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel ContentPanel;
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.TreeView GeoTreeView;
+		private System.Windows.Forms.DataGridView GeoDataGridView;
+		private System.Windows.Forms.ToolStrip GeoToolStrip;
+		private System.Windows.Forms.ToolStripComboBox UnitComboBox;
+		private System.Windows.Forms.ToolStripLabel UnitComboBoxLabel;
+		private System.Windows.Forms.ToolStripComboBox CoordinateComboBox;
+		private System.Windows.Forms.ToolStripLabel CoordinateLabel;
+		private System.Windows.Forms.ToolStripButton ExportButton;
+		private System.Windows.Forms.ToolStripButton ClearListButton;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.GroupBox GeoTreeViewGroupBox;
+		private System.Windows.Forms.ContextMenuStrip GeoContextMenuStrip;
+		private System.Windows.Forms.ToolStripMenuItem SetCoordinateSystemToolStripMenuItem;
+	}
+}
---
+++ Hanbo.WindowsFormsControlLibrary/UserControls/GeoDataUserControl.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//=================================
+using Hanbo.WindowControlWrapper;
+using ViewROI;
+using Hanbo.AOM.Configuration;
+using MeasureModule.ViewModel;
+using MeasureModule;
+
+namespace Hanbo.WindowsFormsControlLibrary.UserControls
+{
+	/// <summary>
+	/// <para>**************</para>
+	/// 量測資料 User Control
+	/// <para>**************</para>
+	/// </summary>
+	public partial class GeoDataUserControl : UserControl
+	{
+		#region ***** 參考座標 *****
+		private BindingList<RefCoordinate> _dfCoordinateBindingList;
+		#endregion
+
+		private GeoDataGridViewManager _geoManager;
+		private ROIController _roiController;
+		public GeoDataUserControl()
+		{
+			InitializeComponent();
+		}
+		public bool Init(ROIController roicontroller, MeasureAssistant mAssistant
+						, double resolution, int roundDigit, string[] inVisibleFields)
+		{
+			var success = true;
+			var model = new List<RefCoordinate>() { 
+								new RefCoordinate() { ID = "", Name = "Default", Desc = "影像座標" },
+						};
+			_dfCoordinateBindingList = new BindingList<RefCoordinate>(model);
+
+			_geoManager = new GeoDataGridViewManager(GeoDataGridView, GeoContextMenuStrip
+											, _dfCoordinateBindingList, inVisibleFields
+											, ConfigurationMM.GeoImageDictionary
+											, resolution, roundDigit, mAssistant);
+			_roiController = roicontroller;
+
+			initGeoContextMenu();
+			initCoordinateComboBox();
+			initLengthUnitComboBox();
+			initClearButton();
+			initExportButton();
+			initGeoTreeView();
+			return success;
+		}
+		public GeoDataGridViewManager GetGeoDataGridViewManager()
+		{
+			return _geoManager;
+		}
+
+		private void initGeoContextMenu()
+		{
+			GeoContextMenuStrip.Opening += GeoContextMenuStrip_Opening;
+		}
+		private void SetCoordinateSystemToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			var menuItem = sender as ToolStripMenuItem;
+			var menuContext = menuItem.GetCurrentParent() as ContextMenuStrip;
+			GeoDataGridViewModel currentModel = getContextMenuGeoDataGridViewModel(menuContext);
+			if (currentModel != null)
+			{
+				var confirmText = String.Format("「{0}」 將設為參考座標？", currentModel.Name);
+				var confirmed = MessageBox.Show(confirmText, "", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes;
+				if (confirmed)
+				{
+					var isExists = _dfCoordinateBindingList.Any(p => p.ID == currentModel.RecordID);
+					if (!isExists)
+					{
+						var nwCoordinate = new RefCoordinate()
+						{
+							ID = currentModel.RecordID,
+							Desc = "",
+							Name = currentModel.Name,
+						};
+						_dfCoordinateBindingList.Add(nwCoordinate);
+						CoordinateComboBox.SelectedItem = nwCoordinate;
+					}
+				}
+			}
+		}
+		private void GeoContextMenuStrip_Opening(object sender, CancelEventArgs e)
+		{
+			var pointTypes = new MeasureType[] { MeasureType.Point, MeasureType.Circle, MeasureType.PointCircle, MeasureType.CrossPoint };
+			var menuContext = sender as ContextMenuStrip;
+			var name = "SetCoordinateSystemToolStripMenuItem";
+			GeoDataGridViewModel model = getContextMenuGeoDataGridViewModel(menuContext);
+			if (model != null)
+			{
+				var menuItem = menuContext.Items[name];//.OfType<ToolStripMenuItem>().SingleOrDefault(p => p.Name == name);
+				var isPointType = (pointTypes.Contains(model.GeoType));
+				menuItem.Enabled = isPointType;
+			}
+		}
+		private GeoDataGridViewModel getContextMenuGeoDataGridViewModel(ContextMenuStrip menuContext)
+		{
+			GeoDataGridViewModel model = null;
+			var dgv = menuContext.SourceControl as DataGridView;
+			if (dgv != null)
+			{
+				var index = dgv.CurrentRow.Index;
+				model = _geoManager.GetAllRecord()[index];
+			}
+
+			var tv = menuContext.SourceControl as TreeView;
+			if (tv != null)
+			{
+				var node = tv.SelectedNode;
+				model = _geoManager.GetAllRecord().SingleOrDefault(p => p.RecordID == node.Name);
+			}
+			return model;
+		}
+
+		private void initGeoTreeView()
+		{
+			_geoManager.SetTreeViewControl(GeoTreeView, ConfigurationMM.GeoTreeViewImageList);
+		}
+
+		private void initExportButton()
+		{
+			ExportButton.Click += (sender, e) =>
+			{
+				//保存報告
+				if (_geoManager.GetRecordCount() == 0)
+				{
+					MessageBox.Show(Hanbo.Resources.Resource.Message_NoData);
+					return;
+				}
+
+				//1。saveFileDialog
+				var saveFileDialog = new SaveFileDialog();
+				saveFileDialog.Filter = "csv (*.csv)|*.csv|all files (*.*)|*.*";
+				saveFileDialog.RestoreDirectory = true;
+				saveFileDialog.InitialDirectory = @"D:\";
+
+				if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+				{
+					var fpath = saveFileDialog.FileName;
+					var sb = new StringBuilder();
+					var rowIndex = 0;
+					//2. Save
+					var exportRecords = _geoManager.GetAllRecord().Select(p => p).ToList();
+					foreach (var model in exportRecords)
+					{
+						rowIndex++;
+						var line = "";
+						if (rowIndex == 1)
+						{
+							line = "流水號,元素,Start.X (pixel),Start.Y (pixel),End.X (pixel),End.Y(pixel),長度(pixel),長度 (" + _geoManager.GetUnit() + ")";
+							sb.AppendLine(line);
+						}
+						line = String.Format(@"{0},{1},{2},{3},{4},{5},{6},{7}",
+							rowIndex, model.GeoType, model.Col1, model.Row1, model.Col2, model.Row2, model.Distance, model.WorldDistance);
+						sb.AppendLine(line);
+					}
+					File.WriteAllText(fpath, sb.ToString(), Encoding.UTF8);
+				}
+			};
+
+		}
+
+		private void initClearButton()
+		{
+			ClearListButton.Click += (sender, e) =>
+			{
+				if (MessageBox.Show(Hanbo.Resources.Resource.Message_DeleteNotice
+								, Hanbo.Resources.Resource.Message_Warning
+								, MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.Yes)
+				{
+					_geoManager.Clear();
+					_roiController.reset();
+					restoreToDefaultCoordinate();
+				}
+			};
+		}
+
+		private void initLengthUnitComboBox()
+		{
+			UnitComboBox.ComboBox.DataSource = Hanbo.AOM.Configuration.ConfigurationMM.LengthUnits;
+			UnitComboBox.ComboBox.ValueMember = "Value";
+			UnitComboBox.ComboBox.DisplayMember = "Name";
+
+			UnitComboBox.SelectedIndex = 1;
+			UnitComboBox.SelectedIndexChanged += (sender, e) =>
+			{
+				var cbox = sender as ToolStripComboBox;
+				if (cbox != null)
+				{
+					var box = cbox.ComboBox;
+					var value = (string)box.SelectedValue;
+					if (_geoManager != null)
+					{
+						_geoManager.SetUnit(value);
+					}
+				}
+			};
+		}
+
+		private void initCoordinateComboBox()
+		{
+			//arrange			
+			BindingSource ds = new BindingSource() { DataSource = _dfCoordinateBindingList };
+
+			//assign
+			CoordinateComboBox.ComboBox.DataSource = ds;
+			CoordinateComboBox.ComboBox.ValueMember = "ID";
+			CoordinateComboBox.ComboBox.DisplayMember = "Name";
+			CoordinateComboBox.ComboBox.SelectedIndex = 0;
+			CoordinateComboBox.ComboBox.SelectedIndexChanged += (sender, e) =>
+			{
+				var cbox = sender as ComboBox;
+				if (cbox != null)
+				{
+					//var box = cbox.ComboBox;
+					var value = (string)cbox.SelectedValue;
+					if (_geoManager != null)
+						_geoManager.SetRefCoordinate(value);
+				}
+			};
+		}
+
+		private void restoreToDefaultCoordinate()
+		{
+			var model = new RefCoordinate() { ID = "", Name = "Default", Desc = "影像座標" };
+			_dfCoordinateBindingList.Clear();
+			_dfCoordinateBindingList.Add(model);
+			//_dfCoordinateBindingList = new BindingList<RefCoordinate>(model);
+			BindingSource ds = new BindingSource() { DataSource = _dfCoordinateBindingList };
+			CoordinateComboBox.ComboBox.DataSource = ds;
+		}
+
+		public void RefreshCoordinate()
+		{
+			var originalSelectedIndex = CoordinateComboBox.ComboBox.SelectedIndex;
+			BindingSource ds = new BindingSource() { DataSource = _dfCoordinateBindingList };
+			CoordinateComboBox.ComboBox.DataSource = ds;
+			var selectedIdx = (originalSelectedIndex <= _dfCoordinateBindingList.Count) ?
+								originalSelectedIndex : 0;
+			CoordinateComboBox.ComboBox.SelectedIndex = selectedIdx;
+		}
+	}
+}
