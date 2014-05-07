91a7ee48a67344e213d1025ae8c28fe314a979ac
joncash <tw.kchsieh@gmail.com>
2014-05-07 09:41:44 +0800

    (+) 幾何操作控制元件

---
+++ Hanbo.WindowsFormsControlLibrary/UserControls/GeometryMeasureControl.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Hanbo.WindowsFormsControlLibrary.UserControls
+{
+	partial class GeometryMeasureControl
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeometryMeasureControl));
+			this.MM_TabControl = new System.Windows.Forms.TabControl();
+			this.MM_TabPage1 = new System.Windows.Forms.TabPage();
+			this.MM_TabPage1_FlowPanel = new System.Windows.Forms.FlowLayoutPanel();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.MM_ToolTip = new System.Windows.Forms.ToolTip(this.components);
+			this.MM_TabControl.SuspendLayout();
+			this.MM_TabPage1.SuspendLayout();
+			this.MM_TabPage1_FlowPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// MM_TabControl
+			// 
+			this.MM_TabControl.Controls.Add(this.MM_TabPage1);
+			resources.ApplyResources(this.MM_TabControl, "MM_TabControl");
+			this.MM_TabControl.Name = "MM_TabControl";
+			this.MM_TabControl.SelectedIndex = 0;
+			// 
+			// MM_TabPage1
+			// 
+			this.MM_TabPage1.BackColor = System.Drawing.SystemColors.Control;
+			this.MM_TabPage1.Controls.Add(this.MM_TabPage1_FlowPanel);
+			resources.ApplyResources(this.MM_TabPage1, "MM_TabPage1");
+			this.MM_TabPage1.Name = "MM_TabPage1";
+			// 
+			// MM_TabPage1_FlowPanel
+			// 
+			resources.ApplyResources(this.MM_TabPage1_FlowPanel, "MM_TabPage1_FlowPanel");
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button1);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button2);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button3);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button4);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button5);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button6);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button7);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button8);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button9);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button10);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button11);
+			this.MM_TabPage1_FlowPanel.Controls.Add(this.button12);
+			this.MM_TabPage1_FlowPanel.Name = "MM_TabPage1_FlowPanel";
+			// 
+			// button5
+			// 
+			this.button5.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.draw_smart_line;
+			resources.ApplyResources(this.button5, "button5");
+			this.button5.Name = "button5";
+			this.MM_ToolTip.SetToolTip(this.button5, resources.GetString("button5.ToolTip"));
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button1
+			// 
+			this.button1.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.draw_point_blue32;
+			resources.ApplyResources(this.button1, "button1");
+			this.button1.Name = "button1";
+			this.MM_ToolTip.SetToolTip(this.button1, resources.GetString("button1.ToolTip"));
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.draw_line_blue32;
+			resources.ApplyResources(this.button2, "button2");
+			this.button2.Name = "button2";
+			this.MM_ToolTip.SetToolTip(this.button2, resources.GetString("button2.ToolTip"));
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.draw_circle_blue32;
+			resources.ApplyResources(this.button3, "button3");
+			this.button3.Name = "button3";
+			this.MM_ToolTip.SetToolTip(this.button3, resources.GetString("button3.ToolTip"));
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.draw_smart_point;
+			resources.ApplyResources(this.button4, "button4");
+			this.button4.Name = "button4";
+			this.MM_ToolTip.SetToolTip(this.button4, resources.GetString("button4.ToolTip"));
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button6
+			// 
+			this.button6.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.distance_blue32;
+			resources.ApplyResources(this.button6, "button6");
+			this.button6.Name = "button6";
+			this.MM_ToolTip.SetToolTip(this.button6, resources.GetString("button6.ToolTip"));
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources._3pointCircle_blue;
+			resources.ApplyResources(this.button7, "button7");
+			this.button7.Name = "button7";
+			this.MM_ToolTip.SetToolTip(this.button7, resources.GetString("button7.ToolTip"));
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.symmetryLine_blue32;
+			resources.ApplyResources(this.button8, "button8");
+			this.button8.Name = "button8";
+			this.MM_ToolTip.SetToolTip(this.button8, resources.GetString("button8.ToolTip"));
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.angle_blue32;
+			resources.ApplyResources(this.button9, "button9");
+			this.button9.Name = "button9";
+			this.MM_ToolTip.SetToolTip(this.button9, resources.GetString("button9.ToolTip"));
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.crossPoint_blue32;
+			resources.ApplyResources(this.button10, "button10");
+			this.button10.Name = "button10";
+			this.MM_ToolTip.SetToolTip(this.button10, resources.GetString("button10.ToolTip"));
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.distanceX_32;
+			resources.ApplyResources(this.button11, "button11");
+			this.button11.Name = "button11";
+			this.MM_ToolTip.SetToolTip(this.button11, resources.GetString("button11.ToolTip"));
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.Image = global::Hanbo.WindowsFormsControlLibrary.Properties.Resources.distanceY_32;
+			resources.ApplyResources(this.button12, "button12");
+			this.button12.Name = "button12";
+			this.MM_ToolTip.SetToolTip(this.button12, resources.GetString("button12.ToolTip"));
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// GeometryMeasureControl
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.MM_TabControl);
+			this.Name = "GeometryMeasureControl";
+			this.MM_TabControl.ResumeLayout(false);
+			this.MM_TabPage1.ResumeLayout(false);
+			this.MM_TabPage1_FlowPanel.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl MM_TabControl;
+		private System.Windows.Forms.TabPage MM_TabPage1;
+		private System.Windows.Forms.FlowLayoutPanel MM_TabPage1_FlowPanel;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.ToolTip MM_ToolTip;
+
+
+	}
+}
---
+++ Hanbo.WindowsFormsControlLibrary/UserControls/GeometryMeasureControl.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//===
+using ViewROI;
+using Hanbo.WindowControlWrapper;
+using MeasureModule;
+namespace Hanbo.WindowsFormsControlLibrary.UserControls
+{
+
+	/*
+	 預期
+	 * 1。可依功能設定檔來顯示功能按鈕及TabPage (進階, Option)
+	 * 2。常用功能按鈕 (進階, Option)
+	 * 3。使用者設定檔 (進階, Option)
+	 */
+
+	//[Designer(typeof(GeometryMeasureControlDesigner))]
+	public partial class GeometryMeasureControl : UserControl
+	{
+		private ROIController _roiController;
+		private HWndCtrl _mView;
+		private GeoDataGridViewManager _geoManager;
+		public GeometryMeasureControl()
+		{
+			InitializeComponent();
+		}
+		/// <summary>
+		/// 初始化
+		/// </summary>
+		/// <param name="roiController"></param>
+		public void Init(ROIController roiController, HWndCtrl mView, GeoDataGridViewManager geoManager)
+		{
+			_roiController = roiController;
+			_mView = mView;
+			_geoManager = geoManager;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//幾何元素 - 點測量
+			_roiController.setROIShape(new ROIRectangle2());
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//幾何元素 - 線測量
+			_roiController.setROIShape(new ROIRectangle2()
+			{
+				ROIMeasureType = MeasureType.Line
+			});
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			//幾何元素 - 圓測量
+			_roiController.setROIShape(new ROICircle());
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			//幾何元素 (點-smart)
+			this._mView.EnableZoomContinue();
+			_roiController.setROIShape(new ViewROI.SmartROIs.SmartPoint());
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			//幾何元素 (線-smart)
+			this._mView.EnableZoomContinue();
+			_roiController.setROIShape(new ViewROI.SmartROIs.PointsLine());
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			//Distance
+			_geoManager.SetCalcuteType(CalcuteType.Distance);
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			//Circle
+			_geoManager.SetCalcuteType(CalcuteType.Point3ToCircle);
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			//Symetry
+			_geoManager.SetCalcuteType(CalcuteType.SymmetryLine);
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			//angle
+			_geoManager.SetCalcuteType(CalcuteType.Angle);
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			//Crosspoint
+			_geoManager.SetCalcuteType(CalcuteType.CrossPoint);
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			//Distance X
+			_geoManager.SetCalcuteType(CalcuteType.DistanceX);
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			//Distance Y
+			_geoManager.SetCalcuteType(CalcuteType.DistanceY);
+		}
+		/*
+		#region Designer===========================================================
+		[DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+		public TabControl MeasureTagControl { get { return this.MeasureFuncTabControl; } }
+		#endregion
+		*/
+	}
+}
