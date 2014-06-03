da6426b8e51fe5ebd6ba3dd8c61441f5791e300c
joncash <tw.kchsieh@gmail.com>
2014-06-03 15:54:19 +0800

    (+)ColorMM.cs (顏色解析) (^) Code Refactoring (ConfigurationMM.cs)

---
+++ Hanbo.AOM.Configuration/ColorMM.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Hanbo.AOM.Configuration
+{
+	/// <summary>
+	/// 顏色轉換管理
+	/// </summary>
+	public static class ColorMM
+	{
+		/// <summary>
+		/// 將顏色字串解析為顏色 (System.Drawing.Color)
+		/// </summary>
+		/// <param name="fromNameOrHtmlString">顏色名稱 or 16 進位顏色字串</param>
+		/// <param name="parseColor">解析後的顏色</param>
+		/// <returns>成功 or 失敗</returns>
+		public static bool TryParseColor(string fromNameOrHtmlString, out Color parseColor)
+		{
+			bool parseSuccess = true;
+			parseColor = Color.Empty;
+			var fromNameColor = Color.FromName(fromNameOrHtmlString);
+			if (fromNameColor.IsKnownColor)
+			{
+				parseColor = fromNameColor;
+			}
+			else
+			{
+				try
+				{
+					parseColor = ColorTranslator.FromHtml("#" + fromNameOrHtmlString);
+				}
+				catch
+				{
+					parseSuccess = false;
+				}
+			}
+			return parseSuccess;
+		}
+	}
+}
--- Hanbo.AOM.Configuration/ConfigurationMM.cs
+++ Hanbo.AOM.Configuration/ConfigurationMM.cs
@@ -26,148 +26,84 @@ namespace Hanbo.AOM.Configuration
 		private static string _systemSettingFile = @"Configuration\Settings.xml";
 		private static XDocument _systemSettingsDoc;
 		#endregion
-		public static XDocument GetConfigurationDoc()
-		{
-			return _systemSettingsDoc;
-		}
-		public static int SubpixThreshold
-		{
-			get
-			{
-				var threshold = 110;//
-				if (_systemSettingsDoc != null)
-				{
-					var xpath = String.Format("{0}/MeasureSettings/SubpixThreshold", _systemSettingsRootName);
-					var elem = getXElementWithXPath(xpath);
-					if (elem != null)
-					{
-						int intValue;
-						if (Int32.TryParse(elem.Value, out intValue))
-						{
-							threshold = intValue;
-						}
-					}
-				}
-				return threshold;
 
-
-			}
-		}
-		public static string UpperLightChannel
-		{
-			get
-			{
-				var channel = "00";
-				if (_systemSettingsDoc != null)
-				{
-					var xpath = String.Format("{0}/LightControl/UpperLight", _systemSettingsRootName);
-					var elem = getXElementWithXPath(xpath);
-					if (elem != null)
-					{
-						channel = elem.Value;
-					}
-				}
-				return channel;
-			}
-		}
-		public static string BottomLightChannel
-		{
-			get
-			{
-				var channel = "01";
-				if (_systemSettingsDoc != null)
-				{
-					var xpath = String.Format("{0}/LightControl/BottomLight", _systemSettingsRootName);
-					var elem = getXElementWithXPath(xpath);
-					if (elem != null)
-					{
-						channel = elem.Value;
-					}
-				}
-				return channel;
-			}
-		}
+		/// <summary>
+		/// 靜態建構子
+		/// </summary>
 		static ConfigurationMM()
 		{
 			loadConfigFile();
 			init();
 		}
-		private static void loadConfigFile()
+
+		/// <summary>
+		/// 取得設定檔案
+		/// </summary>
+		/// <returns>XDocument</returns>
+		public static XDocument GetConfigurationDoc()
 		{
-			_systemSettingsDoc = null;
-			if (File.Exists(_systemSettingFile))
+			return _systemSettingsDoc;
+		}
+
+		/// <summary>
+		/// subpixelThreshold, 預設為 110
+		/// </summary>
+		public static int SubpixThreshold
+		{
+			get
 			{
-				try
+				var threshold = 110;//
+				var xpath = String.Format("{0}/MeasureSettings/SubpixThreshold", _systemSettingsRootName);
+				var settingValue = getSettingValue(xpath, threshold.ToString());
+				int intValue;
+				if (Int32.TryParse(settingValue, out intValue))
 				{
-					_systemSettingsDoc = XDocument.Load(_systemSettingFile);
+					threshold = intValue;
 				}
-				catch (Exception ex)
-				{
-					Hanbo.Log.LogManager.Error("Error, 載入系統設定檔錯誤！");
-					Hanbo.Log.LogManager.Error(ex);
-				}
+				return threshold;
 			}
 		}
-		private static void init()
+
+		/// <summary>
+		/// 上光源頻道, 預設為 00
+		/// </summary>
+		public static string UpperLightChannel
 		{
-			_lengthUnits = new List<UnitViewModel>() { 
-				new UnitViewModel(){ Name = "um", Value = "um"},
-				new UnitViewModel(){ Name = "mm", Value = "mm"},
-				new UnitViewModel(){ Name = "mil", Value = "mil"},
-				new UnitViewModel(){ Name = "inch", Value = "inch"},
-			};
-
-			//小圖示
-			_geoImageDictionary = new Dictionary<string, Bitmap>() { 
-				{"Point", Hanbo.Resources.Resource.draw_point},
-				{"Line", Hanbo.Resources.Resource.draw_line},
-				{"Circle", Hanbo.Resources.Resource.draw_circle},
-				{"Distance", Hanbo.Resources.Resource.distance},
-				{"PointCircle", Hanbo.Resources.Resource._3pointCircle},
-				{"Angle", Hanbo.Resources.Resource.angle},
-				{"SymmetryLine", Hanbo.Resources.Resource.symmetryLine},
-				{"CrossPoint", Hanbo.Resources.Resource.crossPoint},
-				{"DistanceX", Hanbo.Resources.Resource.distanceX},
-				{"DistanceY", Hanbo.Resources.Resource.distanceY},
-				{"Arc", Hanbo.Resources.Resource.draw_arc},
-			};
-
-			_geoTreeviewImageList = new ImageList();
-			foreach (var item in _geoImageDictionary)
+			get
 			{
-				_geoTreeviewImageList.Images.Add(item.Key, item.Value);
+				var channel = "00";
+				var xpath = String.Format("{0}/LightControl/UpperLight", _systemSettingsRootName);
+				return getSettingValue(xpath, channel);
+			}
+		}
+
+		/// <summary>
+		/// 下光源頻道, 預設為 01
+		/// </summary>
+		public static string BottomLightChannel
+		{
+			get
+			{
+				var channel = "01";
+				var xpath = String.Format("{0}/LightControl/BottomLight", _systemSettingsRootName);
+				return getSettingValue(xpath, channel);
 			}
 		}
 
 		/// <summary>
 		/// 長度單位
 		/// </summary>
-		public static List<UnitViewModel> LengthUnits
-		{
-			get
-			{
-				return _lengthUnits;
-			}
-		}
-
-		public static ImageList GeoTreeViewImageList { get { return _geoTreeviewImageList; } }
-		public static Dictionary<string, Bitmap> GeoImageDictionary { get { return _geoImageDictionary; } }
-
+		public static List<UnitViewModel> LengthUnits { get { return _lengthUnits; } }
 
 		/// <summary>
-		/// getXElementWithXPath, Use Default SystemXDoc
+		/// 幾何樹狀顯示圖示集合
 		/// </summary>
-		/// <param name="xpath"></param>
-		/// <returns></returns>
-		private static XElement getXElementWithXPath(string xpath)
-		{
-			XElement xElem = null;
-			if (_systemSettingsDoc != null)
-			{
-				xElem = _systemSettingsDoc.XPathSelectElement(xpath);
-			}
-			return xElem;
-		}
+		public static ImageList GeoTreeViewImageList { get { return _geoTreeviewImageList; } }
+
+		/// <summary>
+		/// 幾何圖示字典
+		/// </summary>
+		public static Dictionary<string, Bitmap> GeoImageDictionary { get { return _geoImageDictionary; } }
 
 		/// <summary>
 		/// NG Color
@@ -177,21 +113,8 @@ namespace Hanbo.AOM.Configuration
 		{
 			get
 			{
-				var channel = Color.Red;//Default
-				if (_systemSettingsDoc != null)
-				{
-					var xpath = String.Format("{0}/JudgeColorSettings/NGColor", _systemSettingsRootName);
-					var elem = getXElementWithXPath(xpath);
-					if (elem != null)
-					{
-						var settingColor = Color.FromName(elem.Value);
-						if (settingColor.IsKnownColor)
-						{
-							channel = settingColor;
-						}
-					}
-				}
-				return channel;
+				var xpath = String.Format("{0}/JudgeColorSettings/NGColor", _systemSettingsRootName);
+				return getColor(xpath, Color.Red);
 			}
 		}
 		/// <summary>
@@ -202,21 +125,8 @@ namespace Hanbo.AOM.Configuration
 		{
 			get
 			{
-				var channel = Color.Lime;//Default
-				if (_systemSettingsDoc != null)
-				{
-					var xpath = String.Format("{0}/JudgeColorSettings/OKColor", _systemSettingsRootName);
-					var elem = getXElementWithXPath(xpath);
-					if (elem != null)
-					{
-						var settingColor = Color.FromName(elem.Value);
-						if (settingColor.IsKnownColor)
-						{
-							channel = settingColor;
-						}
-					}
-				}
-				return channel;
+				var xpath = String.Format("{0}/JudgeColorSettings/OKColor", _systemSettingsRootName);
+				return getColor(xpath, Color.Lime);
 			}
 		}
 
@@ -248,5 +158,101 @@ namespace Hanbo.AOM.Configuration
 			}
 			return success;
 		}
+
+		#region private methods
+		private static void loadConfigFile()
+		{
+			_systemSettingsDoc = null;
+			if (File.Exists(_systemSettingFile))
+			{
+				try
+				{
+					_systemSettingsDoc = XDocument.Load(_systemSettingFile);
+				}
+				catch (Exception ex)
+				{
+					Hanbo.Log.LogManager.Error("Error, 載入系統設定檔錯誤！");
+					Hanbo.Log.LogManager.Error(ex);
+				}
+			}
+		}
+		private static void init()
+		{
+			//長度單位
+			_lengthUnits = new List<UnitViewModel>() { 
+				new UnitViewModel(){ Name = "um", Value = "um"},
+				new UnitViewModel(){ Name = "mm", Value = "mm"},
+				new UnitViewModel(){ Name = "mil", Value = "mil"},
+				new UnitViewModel(){ Name = "inch", Value = "inch"},
+			};
+
+			//小圖示
+			_geoImageDictionary = new Dictionary<string, Bitmap>() { 
+				{"Point", Hanbo.Resources.Resource.draw_point},
+				{"Line", Hanbo.Resources.Resource.draw_line},
+				{"Circle", Hanbo.Resources.Resource.draw_circle},
+				{"Distance", Hanbo.Resources.Resource.distance},
+				{"PointCircle", Hanbo.Resources.Resource._3pointCircle},
+				{"Angle", Hanbo.Resources.Resource.angle},
+				{"SymmetryLine", Hanbo.Resources.Resource.symmetryLine},
+				{"CrossPoint", Hanbo.Resources.Resource.crossPoint},
+				{"DistanceX", Hanbo.Resources.Resource.distanceX},
+				{"DistanceY", Hanbo.Resources.Resource.distanceY},
+				{"Arc", Hanbo.Resources.Resource.draw_arc},
+			};
+
+			_geoTreeviewImageList = new ImageList();
+			foreach (var item in _geoImageDictionary)
+			{
+				_geoTreeviewImageList.Images.Add(item.Key, item.Value);
+			}
+		}
+
+		private static string getSettingValue(string xpath, string defaultValue)
+		{
+			var settingValue = defaultValue;
+			if (_systemSettingsDoc != null)
+			{
+				var elem = getXElementWithXPath(xpath);
+				if (elem != null)
+				{
+					settingValue = elem.Value;
+				}
+			}
+			return settingValue;
+		}
+
+		/// <summary>
+		/// getXElementWithXPath, Use Default SystemXDoc
+		/// </summary>
+		/// <param name="xpath"></param>
+		/// <returns></returns>
+		private static XElement getXElementWithXPath(string xpath)
+		{
+			XElement xElem = null;
+			if (_systemSettingsDoc != null)
+			{
+				xElem = _systemSettingsDoc.XPathSelectElement(xpath);
+			}
+			return xElem;
+		}
+		private static Color getColor(string xpath, Color defaultColor)
+		{
+			Color settingColor = defaultColor;
+			if (_systemSettingsDoc != null)
+			{
+				var elem = getXElementWithXPath(xpath);
+				if (elem != null)
+				{
+					Color parseColor;
+					if (ColorMM.TryParseColor(elem.Value, out parseColor))
+					{
+						settingColor = parseColor;
+					}
+				}
+			}
+			return settingColor;
+		}
+		#endregion
 	}
 }
