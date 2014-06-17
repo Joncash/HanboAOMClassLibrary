d33996026d9c7c0a611fc9141732f56bec88969f
joncash <tw.kchsieh@gmail.com>
2014-06-17 18:58:01 +0800

    (+) 新增 HWindowSettingViewModel.cs (視窗設定參數)

---
+++ Hanbo.Models/HWindowSettingViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hanbo.Models
+{
+	/// <summary>
+	/// HWCtrl 設定值 ViewModel
+	/// </summary>
+	public class HWindowSettingViewModel
+	{
+		public HWindowSettingViewModel()
+		{
+			HLines = 2;
+			VLines = 2;
+			GridLineColor = "red";
+			ShowGridLine = false;
+			ShowAccuracyAreaGridLine = false;
+			AccuracyAreaGridLineColor = "lime green";
+		}
+		public int HLines { get; set; }
+		public int VLines { get; set; }
+		public string GridLineColor { get; set; }
+		public bool ShowGridLine { get; set; }
+
+		public bool ShowAccuracyAreaGridLine { get; set; }
+		public string AccuracyAreaGridLineColor { get; set; }
+	}
+}
