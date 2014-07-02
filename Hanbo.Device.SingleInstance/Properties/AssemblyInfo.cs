97599184db39090ac377f1f8cb658af6cfff3ce9
joncash <tw.kchsieh@gmail.com>
2014-07-02 21:00:50 +0800

    (^) Hanbo.Device.SingleInstance,  update AssemblyInfo.cs, 1.0.0.2

--- Hanbo.Device.SingleInstance/Properties/AssemblyInfo.cs
+++ Hanbo.Device.SingleInstance/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Hanbo.Device.SingleInstance")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hanbo Taiwan")]
 [assembly: AssemblyProduct("Hanbo.Device.SingleInstance")]
 [assembly: AssemblyCopyright("Copyright ©  2013-2014 Hanbo Taiwan")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
