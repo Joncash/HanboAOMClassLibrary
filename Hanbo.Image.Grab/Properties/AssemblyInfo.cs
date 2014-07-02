606ebccf2df0de26073ae39dde5c76742e94e5ac
joncash <tw.kchsieh@gmail.com>
2014-07-02 20:59:02 +0800

    (^) Update AssemblyInfo.cs, version 1.0.0.1

--- Hanbo.Image.Grab/Properties/AssemblyInfo.cs
+++ Hanbo.Image.Grab/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hanbo.Image.Grab")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Image Acquisition Library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Hanbo Taiwan")]
 [assembly: AssemblyProduct("Hanbo.Image.Grab")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2014 Hanbo Taiwan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
